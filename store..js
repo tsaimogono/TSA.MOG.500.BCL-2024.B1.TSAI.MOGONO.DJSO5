7a936a0696532a03150140253e9c856dbbe62f78
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-24 14:15:00 +0200

    dispatch function to update the state by executing action function

--- store..js
+++ store..js
@@ -11,3 +11,9 @@ export let state = initialState;
  * @type {Array<Function>}
  */
 export const subscribers = [];
+
+/**
+ * Dispatch a function to update the state by calling an action creator and notifying all subscribers
+ * @param {Function} action - Execute the action function and get the next state
+ * @returns {void}
+ */
