0ae74bcb0b43003d48b4e5ace1fdeb7c5be247c1
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-05-24 13:51:45 +0200

    function retrieves the current state of your application and  dispatches an action from the Redux store

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 import { dispatch } from "./store.js";
 import { getState, addAction, subtractAction, resetAction } from "./actions.js";
+
+// Get the initial state
+getState();
+
+// Dispatch the addAction twice
+dispatch(addAction);
+dispatch(addAction);
